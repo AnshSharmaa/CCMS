90f77b24e834b67bc33c60bf231038af12f0f2a8
Akshay Godara <77118299+akshaygodara29@users.noreply.github.com>
2021-05-21 09:31:33 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,41 @@ import React from "react"
 
 const App = () => {
   return (
+    <>
     <div>
-      <h1>App</h1>
+     <form action="/action_page.php">
+    <div class="row">
+      <div class="col-25">
+      </div>
+      <div class="col-75">
+        <input type="text" id="fname" name="firstname" placeholder="Title">
+      </div>
     </div>
+    <div class="row">
+      <div class="col-25">
+      </div>
+      <div class="col-75">
+        <input type="text" id="lname" name="lastname" placeholder="Author Name">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-25">
+      </div>
+      <div class="col-75">
+        <textarea id="content" name="subject" placeholder="Content" style="height:200px"></textarea><br>
+      </div>
+    <div class="row">
+      <input type="Date" placeholder="Date">
+    </div>
+</div>
+
+<label class="switch">
+  <input type="checkbox" checked>
+  <span class="slider round"></span>
+Post
+</label>
+    </div>
+</>
   )
 }
 
