7b66f37ac3e53130d1ae0f12f485bb64fd5cf03a
Akshay Godara <77118299+akshaygodara29@users.noreply.github.com>
2021-05-21 09:37:10 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,34 +5,34 @@ const App = () => {
     <>
     <div>
      <form action="/action_page.php">
-    <div class="row">
-      <div class="col-25">
+    <div className="row">
+      <div className="col-25">
       </div>
-      <div class="col-75">
+      <div className="col-75">
         <input type="text" id="fname" name="firstname" placeholder="Title"/>
       </div>
     </div>
-    <div class="row">
-      <div class="col-25">
+    <div className="row">
+      <div className="col-25">
       </div>
-      <div class="col-75">
+      <div className="col-75">
         <input type="text" id="lname" name="lastname" placeholder="Author Name"/>
       </div>
     </div>
-    <div class="row">
-      <div class="col-25">
+    <div className="row">
+      <div className="col-25">
       </div>
-      <div class="col-75">
+      <div className="col-75">
         <textarea id="content" name="subject" placeholder="Content" style="height:200px"></textarea><br>
       </div>
-    <div class="row">
+    <div className="row">
       <input type="Date" placeholder="Date"/>
     </div>
 </div>
 
-<label class="switch">
+<label className="switch">
   <input type="checkbox" checked/>
-  <span class="slider round"></span>
+  <span className="slider round"></span>
 Post
 </label>
     </div>
