a5e0c5ece15e6dd3e4300b117c0b2fad22847634
Akshay Godara <77118299+akshaygodara29@users.noreply.github.com>
2021-05-21 09:34:39 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import './App.css';
 const App = () => {
   return (
     <>
@@ -9,14 +9,14 @@ const App = () => {
       <div class="col-25">
       </div>
       <div class="col-75">
-        <input type="text" id="fname" name="firstname" placeholder="Title">
+        <input type="text" id="fname" name="firstname" placeholder="Title"/>
       </div>
     </div>
     <div class="row">
       <div class="col-25">
       </div>
       <div class="col-75">
-        <input type="text" id="lname" name="lastname" placeholder="Author Name">
+        <input type="text" id="lname" name="lastname" placeholder="Author Name"/>
       </div>
     </div>
     <div class="row">
@@ -26,12 +26,12 @@ const App = () => {
         <textarea id="content" name="subject" placeholder="Content" style="height:200px"></textarea><br>
       </div>
     <div class="row">
-      <input type="Date" placeholder="Date">
+      <input type="Date" placeholder="Date"/>
     </div>
 </div>
 
 <label class="switch">
-  <input type="checkbox" checked>
+  <input type="checkbox" checked/>
   <span class="slider round"></span>
 Post
 </label>
