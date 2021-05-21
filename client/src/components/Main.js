6501e67d1a23feb6cdcc6d6ca17608ba681fdb10
GH-Gursirman <sohanpalgursimransingh@gmail.com>
2021-05-21 12:57:59 +0530

    cards added

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box , Card, CardContent, Container, Typography} from '@material-ui/core';
+import { Box , Card, Grid, CardContent, Container, Typography} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { pink } from "@material-ui/core/colors";
 import image from '../Images/main.png';
@@ -16,11 +16,18 @@ const useStyles = makeStyles({
         backgroundSize: 'cover', 
         backgroundRepeat: 'no-repeat',
     },
+    LOGO : {
+        flex: 1,
+    },
     feat : {
         width: "100%"
     },
-    cards : {
-        flex:1,
+    card : {
+        flex: 1,
+    },
+    started : {
+        backgroundColor: pink[100],
+        height: "200px",
     }
   });
 
@@ -28,42 +35,61 @@ const Main = () => {
     const classes = useStyles();
     return (
         <div className={classes.container}>
-            <Box component="div" className={classes.MainDiv}>
+            <Box component="div" className={classes.MainDiv} justifyContent="space-in-between">
+                <Box className={classes.LOGO}>
+                    LOGO
+                </Box>
+                <Typography color="inherit">
+                    Hi, Welcome to Custom CCMS
+                </Typography>
             </Box>
             <Box component="div" className={classes.feat}>
                 <Typography align="center" variant="h5">Features</Typography>
             </Box>
             <Container className={classes.card}>
-            <Card width={1/4}>
-                <CardContent>
-                    <Typography>
-                        Create
-                    </Typography>
-                </CardContent>
-            </Card>
-            <Card width={1/4}>
-                <CardContent>
-                    <Typography>
-                        Update
-                    </Typography>
-                </CardContent>
-            </Card>
-            <Card width={1/4} className={classes.card}>
-                <CardContent>
-                    <Typography>
-                        Delete
-                    </Typography>
-                </CardContent>
-            </Card>
-            <Card width={1/4} className={classes.card}>
-                <CardContent>
-                    <Typography>
-                        2 Types of entities
-                    </Typography>
-                </CardContent>
-            </Card>
-            </Container>
+                <Grid container>
+                    <Grid item sm={3}>
+                    <Card width={1/4}>
+                        <CardContent>
+                            <Typography>
+                                Create
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    </Grid>
+                    <Grid item sm={3}>
+                    <Card width={1/4}>
+                        <CardContent>
+                            <Typography>
+                                Update
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    </Grid>
+                    <Grid item sm={3}>
+                    <Card width={1/4} className={classes.card}>
+                        <CardContent>
+                            <Typography>
+                                Delete
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    </Grid>
+                    <Grid item sm={3}>
+                    <Card width={1/4} className={classes.card}>
+                        <CardContent>
+                            <Typography>
+                                2 Types of entities
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    </Grid>
+                </Grid>
             
+            </Container>
+            <Box component="div" className={classes.started}>
+                <Typography align="center" variant="h5">Getting Started</Typography>
+            </Box>
         </div>
         
         
