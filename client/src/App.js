e1cb4f0691baa975cd9d55dea8b99885c230d4e5
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2021-05-25 10:46:07 +0530

    auth client fix

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import "./App.css"
 import Header from "./Components/Header/header"
 import Home from "./Pages/Home/Home"
 import Data from "./Pages/Data/Data"
+import Auth from "./Components/Auth/Auth"
 
 const App = () => {
   return (
@@ -16,6 +17,8 @@ const App = () => {
           <Route path="/" exact component={() => <Home />} />
 
           <Route path="/data" exact component={() => <Data />} />
+
+          <Route path="/login" exact component={() => <Auth />} />
         </Switch>
       </Router>
     </div>
