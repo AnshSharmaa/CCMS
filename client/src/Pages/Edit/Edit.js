c478cc1f90b36f1a002011e4818c007db0f145c5
GH-Gursirman <sohanpalgursimransingh@gmail.com>
2021-06-02 13:30:55 +0530

    date fixed

--- client/src/Pages/Edit/Edit.js
+++ client/src/Pages/Edit/Edit.js
@@ -12,9 +12,24 @@ const useStyles = makeStyles({
 const Edit = () => {
   const stylesClasses = styles()
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+  var dateHash = {
+    Jan : '01',
+    Feb: '02',
+    Mar: '03',
+    Apr: '04',
+    May: '05',
+    Jun: '06',
+    Jul: '07',
+    Aug: '08',
+    Sep: '09',
+    Oct: '10',
+    Nov: '11',
+    Dec: '12'
+   };
   const location = useLocation()
   const { post } = location.state
+  const postD = post.Date.split(" ");
+  const date = postD[3]+"-"+dateHash[postD[1]]+"-"+postD[2]
   console.log(post)
   return (
     <form>
@@ -76,7 +91,7 @@ const Edit = () => {
                   marginBottom: "10px",
                   maxWidth: "150px",
                 }}
-                defaultValue={post.Date}
+                defaultValue={date}
                 type="date"
                 InputLabelProps={{
                   shrink: true,
