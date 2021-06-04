d91ec609356c8c74a3b85f0520787bb06bb33a01
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2021-06-04 21:51:29 +0530

    styling fix

--- client/src/components/Form/PostForm.js
+++ client/src/components/Form/PostForm.js
@@ -5,7 +5,7 @@ import styles from "../../styles/styles"
 import mcgpalette0 from "../../styles/colors"
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 import {createPost} from "../../api/api"
-
+import "./PostForm.css"
 const useStyles = makeStyles({
   createPostAccord: {
     marginTop: "20px",
@@ -18,7 +18,7 @@ const useStyles = makeStyles({
   customTextField: {
     marginTop: "10px",
     marginBottom: "10px",
-    maxWidth: "400px",
+    maxWidth: "97%",
     padding: 5,
     borderRadius: 5,
     backgroundColor: mcgpalette0["background-color-3"],
@@ -72,11 +72,16 @@ const PostForm = (user) => {
         <AccordionDetails>
           <Grid container direction='column' justify='space-evenly'>
             <form style={{width: "100%"}} onSubmit={handleSubmitPost}>
-              <Typography style={{color: mcgpalette0["text-color-light"]}}>Title</Typography>
-              <TextField required className={classes.customTextField} name='Title' onChange={(e) => setPostData({...postData, Title: e.target.value})} />
-
-              <Typography style={{color: mcgpalette0["text-color-light"]}}>Author</Typography>
-              <TextField required className={classes.customTextField} name='Author' onChange={(e) => setPostData({...postData, Author: e.target.value})} />
+              <div style={{display: "flex", justifyContent: "space-evenly", flexWrap: "nowrap"}}>
+                <div className='subRapper' style={{width: "100%"}}>
+                  <Typography style={{color: mcgpalette0["text-color-light"]}}>Title</Typography>
+                  <TextField required className={classes.customTextField} name='Title' onChange={(e) => setPostData({...postData, Title: e.target.value})} />
+                </div>
+                <div className='subRapper' style={{width: "100%"}}>
+                  <Typography style={{color: mcgpalette0["text-color-light"]}}>Author</Typography>
+                  <TextField required className={classes.customTextField} name='Author' onChange={(e) => setPostData({...postData, Author: e.target.value})} />
+                </div>
+              </div>
 
               <Typography style={{color: mcgpalette0["text-color-light"]}}>Content</Typography>
               <TextField required fullWidth multiline rows={4} className={classes.customTextField} name='Content' onChange={(e) => setPostData({...postData, Content: e.target.value})} />
@@ -84,8 +89,8 @@ const PostForm = (user) => {
               <Typography style={{color: mcgpalette0["text-color-light"]}}>Date</Typography>
               <TextField required style={{marginTop: "10px", marginBottom: "10px", maxWidth: "150px"}} className={classes.customTextField} type='date' InputLabelProps={{shrink: true}} name='Date' onChange={(e) => setPostData({...postData, Date: e.target.value})} />
 
-              <div style={{marginLeft: "auto"}}>
-                <Button className={stylesClasses.customButtonContainedSecondary} type='submit'>
+              <div>
+                <Button className={stylesClasses.customButtonContainedSecondary} type='submit' style={{width: "160px", padding: "10px 0", fontWeight: "600", margin: "0"}}>
                   Submit
                 </Button>
               </div>
--- client/src/index.js
+++ client/src/index.js
@@ -4,3 +4,7 @@ import "./index.css"
 import App from "./App"
 
 ReactDom.render(<App />, document.getElementById("root"))
+
+// user auth for delete
+// user auth for edit(alert: not signed it)
+// post placement rip
